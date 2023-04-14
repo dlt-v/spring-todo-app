c92fbb0151c00b8dac96a0fcee1e538c6c55c6fc
Delta.v <tmm.michalski@gmail.com>
2023-04-14 13:52:15 +0200

    add first simple response

---
+++ src/main/java/com/michalski/todoapp/hello/SayHelloController.java
@@ -0,0 +1,15 @@
+package com.michalski.todoapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+    @RequestMapping("hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello!";
+    }
+}
